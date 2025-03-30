304fb3e5b60aae51006460a465974ef66e96859f
Денис <d-is-for-diamond@yandex.ru>
2025-03-30 18:16:03 +0300

    seleniumWebDriver-B50 первый коммит

---
+++ src/test/java/org/example/FirstTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import junit.framework.Test;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class FirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnG")).click();
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+}
