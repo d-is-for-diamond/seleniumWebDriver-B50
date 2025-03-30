7a2284092661fef821d63c02d89dcbddd648ba04
Денис <d-is-for-diamond@yandex.ru>
2025-03-30 18:34:49 +0300

    seleniumWebDriver-B50 Коммит первого задания 2

--- src/test/java/org/example/FirstTest.java
+++ src/test/java/org/example/FirstTest.java
@@ -4,9 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class FirstTest extends TestBase {
     private WebDriver driver;
