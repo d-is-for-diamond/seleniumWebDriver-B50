b8682bcd5fac24fcdc14611d1f51e597a9e7485a
Денис <d-is-for-diamond@yandex.ru>
2025-04-02 00:39:37 +0300

    seleniumWebDriver-B50 Задание 6. Сценарий, проходящий по всем разделам админки 2

---
+++ src/test/java/org/example/FourthTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class FourthTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofMillis(10000));
+    }
+
+    @Test //Задание 7. Сценарий, проверяющий наличие стикеров у товаров
+    public void myThirdTest() {
+        driver.get("http://localhost:8080/litecart/en/rubber-ducks-c-1/");
+        List<WebElement> products = driver.findElements(By.cssSelector(".listing-wrapper.products li.product"));
+        products.forEach(product -> product.findElement(By.cssSelector("a div.sticker")).isDisplayed());
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+        driver = null;
+    }
+}
